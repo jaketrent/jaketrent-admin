8aabe1a2f5ec5152217ed32658d008d93770cb39
jaketrent <trent.jake@gmail.com>
2014-07-27 09:34:04 -0600

    Commit

--- Brocfile.js
+++ Brocfile.js
@@ -2,7 +2,7 @@
 
 var filterBrowserify = require('broccoli-browserify')
 var filterReact = require('broccoli-react')
-var filterSass = require('broccoli-sass')
+//var filterSass = require('broccoli-sass')
 var freeze = Object.freeze
 var mergeTrees = require('broccoli-merge-trees')
 var pickFiles = require('broccoli-static-compiler')
