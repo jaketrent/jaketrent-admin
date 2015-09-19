e713ecb0c0c9c04a83270f91d1002c355a6c8c4e
jaketrent <trent.jake@gmail.com>
2015-09-19 11:30:38 -0600

    Rm sidenav from layout

--- client/books/layout.js
+++ client/books/layout.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import styleable from 'react-styleable'
 
 import BooksSideNav from './side-nav'
+import css from './layout.css'
 import SiteLayout from '../common/components/layout'
 
 const { arrayOf, object } = React.PropTypes
 
+@styleable(css)
 export default class BooksLayout extends React.Component {
   static propTypes = {
     books: arrayOf(object)
@@ -15,8 +18,7 @@ export default class BooksLayout extends React.Component {
   render() {
     return (
       <SiteLayout>
-        <BooksSideNav books={this.props.books} />
-        <main>
+        <main className={this.props.css.root}>
           {this.props.children}
         </main>
       </SiteLayout>
